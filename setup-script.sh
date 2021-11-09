d5621a6d5b1a3a6aa4d9f1a596b4423c1f7b1f5f
EC2 Default User <ec2-user@ip-172-31-19-132.ec2.internal>
2021-11-09 11:28:24 +0000

    Docker & kubectl setups added

--- setup-script.sh
+++ setup-script.sh
@@ -4,3 +4,16 @@
 
 # update OS
 sudo yum update -y
+
+# install docker
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+docker info
+
+# install kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl version
